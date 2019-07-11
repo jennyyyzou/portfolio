36998c1d65d767df06506aa19f087935085a5314
Yuyan Zou <jennyyyzou@hotmail.com>
2019-07-11 00:03:09 -0700

    full page is fully implemented

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,7 @@
-  $(document).ready(function(){
-    $("#about").click(function(){
-      $("#landing-long-content").fadeIn('slow');
-      $("#portrait").hide();
-    });
+// SHOWING ABOUT CONTENT
+$(document).ready(function(){
+  $('#about').click(function(){
+    $('#landing-long-content').fadeIn('slow');
+    $('#portrait').hide();
   });
+});
