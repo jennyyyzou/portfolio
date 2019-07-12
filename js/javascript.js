42fcf213679414c2a9b464892d831fb89e307a8b
Yuyan Zou <jennyyyzou@hotmail.com>
2019-07-11 22:28:59 -0700

    Landing page section is overall done without responsiveness

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,41 @@
-function openContent() {
-  document.getElementById("landing-long-content").style.width = "100%";
+/**************************************************
+SCROLL TO TOP AFTER REFRESH
+**************************************************/
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
 }
 
-function closeContent() {
-  document.getElementById("landing-long-content").style.width = "0%";
-}
+/**************************************************
+OVERLAY TOGGLE
+**************************************************/
+window.addEventListener("load", function(){
+  var open = document.getElementById("overlay-open"),
+      close = document.getElementById("overlay-close"),
+      over = document.getElementById("overlay");
+
+  open.addEventListener("click", function(){
+    over.classList.add("show");
+  });
+  close.addEventListener("click", function(){
+    over.classList.remove("show");
+  });
+  close.addEventListener("click", function(){
+    over.classList.remove("show");
+  });
+});
+
+/**************************************************
+SMOOTH SCROLL
+**************************************************/
+$(document).ready(function(){
+$("a").on("click",function(event){
+  if(""!==this.hash){
+    event.preventDefault();
+    var hash=this.hash;
+    $("html, body").animate({
+      scrollTop:$(hash).offset().top},800,function(){
+        window.location.hash=hash;
+      });
+    }
+  });
+});
